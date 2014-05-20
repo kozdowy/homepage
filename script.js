94be4e80b4fad175b1cefe2750ac8ed205c43498
kozdowy <kozdowy@umich.edu>
2014-05-20 11:22:13 -0400

    logging linkname and url

--- script.js
+++ script.js
@@ -30,7 +30,9 @@ function populateLinks(){
     var linkElements = document.getElementsByTagName("td");
     for (var i = 0; i < 8; i++){
         linkName = links[i].getElementsByTagName("name")[0].nodeValue;
+        Console.log(linkName);
         linkURL = links[i].getElementsByTagName("url")[0].nodeValue;
+        Console.log(linkURL);
         linkElements[i].innerHTML =
             "<a href=" + linkURL + ">" +
                 "<div class='quicklink'>" +
